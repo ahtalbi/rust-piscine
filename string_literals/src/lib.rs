750fba5d0a954ce7741f6f1c09a2cce643959127
ahtalbi <obetox1@gmail.com>
2026-06-27 05:06:15 +0100

    string literals

---
+++ string_literals/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn is_empty(v: &str) -> bool {
+    v.is_empty()
+}
+
+pub fn is_ascii(v: &str) -> bool {
+    v.is_ascii()
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    match v.find(pat) {
+        Some(i) => i,
+        None => v.len(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
